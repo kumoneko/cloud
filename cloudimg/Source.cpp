2edd2d4b1bdcc49d976b690e87df29c442ecbd60
kumoneko <cat2602063@gmail.com>
2016-09-28 23:06:14 +0800

    Update Source.cpp

--- cloudimg/Source.cpp
+++ cloudimg/Source.cpp
@@ -12,78 +12,7 @@ using namespace cv;
 using namespace std;
 
 
-void table(char name[20]) {
-	
-		IplImage *src_image = 0, *dst_image = 0;
-		CvMat *lut_mat;
-		lut_mat = cvCreateMatHeader(1, 256, CV_8UC1);
-		uchar lut[256];
-		for (int i = 0; i < 256; i++) {
-			lut[i] = 255 - i;
-		}
-		src_image = cvLoadImage(name);
-		dst_image = cvCloneImage(src_image);
-		cvSetData(lut_mat, lut, 0);
-		cvLUT(src_image, dst_image, lut_mat);
-		cvNamedWindow("src_image", 2);
-		cvNamedWindow("dst_image", 2);
-		cvShowImage("src_image", src_image);
-		cvShowImage("dst_image", dst_image);
-		cvWaitKey(0);
-		cvReleaseImage(&src_image);
-		cvReleaseImage(&dst_image);
-		
-		
-}
 
-void find(char name[20])
-{
-	int largest_area = 0;
-	int largest_contour_index = 0;
-	Rect bounding_rect;
-
-	Mat src = imread(name); // Load source image
-	Mat thr(src.rows, src.cols, CV_8UC1);
-	Mat dst(src.rows, src.cols, CV_8UC3, Scalar::all(0));
-	cvtColor(src, thr, CV_BGR2GRAY); // Convert to gray
-	threshold(thr, thr, 25, 255, THRESH_BINARY); // Threshold the gray
-
-	vector<vector<Point>> contours; // Vector for storing contour
-	vector<Vec4i> hierarchy;
-
-	findContours(thr, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE); // Find the contours in the image
-
-	Scalar color(0, 0, 255);
-
-	for (int i = 0; i < contours.size(); i++) // Iterate through each contour
-	{
-		double a = contourArea(contours[i], false); // Find the area of contour
-		if (a > largest_area) {
-			largest_area = a;
-			largest_contour_index = i; // Store the index of largest contour
-			bounding_rect = boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
-		}
-
-	}
-	for (int i = 0; i < contours.size(); i++) // Iterate through each contour
-	{
-		if (i != largest_contour_index) {
-			drawContours(dst, contours, i, color, CV_FILLED, 8, hierarchy);
-		}
-	}
-
-	Scalar color2(255, 0, 0);
-	drawContours(dst, contours, largest_contour_index, color2, 5, 8, hierarchy); // Draw the largest contour using previously stored index.
-
-	rectangle(src, bounding_rect, Scalar(0, 255, 0), 1, 8, 0);
-	
-	imshow("Happy Thread", thr);
-	imshow("Happy Source", src);
-	imshow("Happy Largest Contour", dst);
-
-	// Wait for a keystroke in the window
-	waitKey(0);
-}
 
 void hough(char name[20])
 {
@@ -128,16 +57,7 @@ void hough(char name[20])
 	*/
 
 
-	///src erode OR dilate code
-	/*
-	Mat erodeStruct = getStructuringElement(MORPH_RECT, Size(2, 2)); 
-	erode(src_gray, src_gray, erodeStruct);
-	Mat dilateStruct = getStructuringElement(MORPH_CROSS, Size(6, 6));
-	dilate(src_gray, src_gray, dilateStruct);
-	GaussianBlur(src_gray, src_gray, Size(9, 9), 15, 15);
 	
-	GaussianBlur(src_gray, src_gray, Size(9, 9), 15, 15);
-	*/
 	
 	vector<Vec3f> circles;
 	
@@ -192,32 +112,11 @@ int main()
 		sprintf(file, "201609%02d%02d%02d.jpg", day, hr, min);
 		hough(file);
 
-//		pic = imread(file, CV_LOAD_IMAGE_GRAYSCALE);
-//		threshold(pic, dst, 180, 255, THRESH_BINARY);
-		
-		
-//		namedWindow("threshold", WINDOW_NORMAL);
-		
-//		imshow("origin", pic);
-//		imshow("threshold", dst);
-//		waitKey(0);
-
-
-
 
 		min = 30;
 		sprintf(file, "201609%02d%02d%02d.jpg", day, hr, min);
 		hough(file);
 		
-//		pic = imread(file, CV_LOAD_IMAGE_GRAYSCALE);
-//		threshold(pic, dst, 180, 255, THRESH_BINARY);
-		
-		
-
-//		namedWindow("threshold", WINDOW_NORMAL);
-//		imshow("origin", pic);
-//		imshow("threshold", dst);
-//		waitKey(0);
 
 
 	
